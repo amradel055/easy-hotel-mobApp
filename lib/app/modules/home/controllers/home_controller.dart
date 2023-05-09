5a1ccc3aebf7df9e4bb8f623551719b79ad77898
GamalAtya <gamalatya55@gmail.com>
2023-05-09 05:35:59 +0300

    notifications

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -4,9 +4,13 @@ import 'package:easy_hotel/app/modules/my_account/acoount_homepage/controllers/m
 import 'package:easy_hotel/app/modules/my_account/acoount_homepage/views/my_account_view.dart';
 import 'package:easy_hotel/app/modules/rooms/rooms_homepage/controllers/rooms_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
+import '../../../../main.dart';
+import '../../../core/utils/user_manager.dart';
 import '../../food_cart/controllers/food_cart_controller.dart';
 import '../../rooms/rooms_homepage/views/rooms_view.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 
 class HomeController extends GetxController {
@@ -17,13 +21,43 @@ class HomeController extends GetxController {
     HotelSearchView(),
     MyAccountView(),
   ];
+  final user = UserManager();
 
   @override
   void onInit() {
     Get.isRegistered<HotelSearchController>() ? Get.find<HotelSearchController>() : Get.put(HotelSearchController());
     Get.isRegistered<MyAccountController>() ? Get.find<MyAccountController>() : Get.put(MyAccountController());
     Get.isRegistered<FoodCartController>() ? Get.find<FoodCartController>() : Get.put<FoodCartController>(FoodCartController());
+    final FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+      firebaseMessaging.subscribeToTopic("customers/${user.user!.id!}");
+       WidgetsBinding.instance.addPostFrameCallback((_) async {
+      RemoteMessage? initialMessage = await FirebaseMessaging.instance.getInitialMessage();
+    });
+    var initialzationSettingsAndroid = const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettings = InitializationSettings(android: initialzationSettingsAndroid);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
 
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification!.android;
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channelDescription: channel.description,
+                icon: android.smallIcon,
+                importance: Importance.max,
+                priority: Priority.max,
+                playSound: true,
+              ),
+            ));
+      }
+    });
     // Get.isRegistered<RoomsController>() ? Get.find<RoomsController>() : Get.put(RoomsController());
     super.onInit();
   }
